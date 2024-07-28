c5f7b78d710b85a886642cc0d29ffdadf09de5ee
Baran Sütçü <baran.sutcu@tigahealth.com>
2024-07-28 17:43:35 +0300

    Rename SimplebankingApplication to SimpleBankingApplication
    Renamed the main application class for consistent camel case usage. Added the scanBasePackages attribute to the @SpringBootApplication annotation to specify the base package.

--- src/test/java/com/baran/simplebanking/SimpleBankingApplicationTests.java
+++ src/test/java/com/baran/simplebanking/SimpleBankingApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SimplebankingApplicationTests {
+class SimpleBankingApplicationTests {
 
     @Test
     void contextLoads() {
